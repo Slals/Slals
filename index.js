ec656bb2df60db18c76088ab18c9873042752cc8
Slals <jonathan@blocs.fr>
2021-01-27 04:15:56 +0100

    Adds workflow init

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("isomorphic-unfetch");
 const { promises: fs } = require("fs");
 const path = require("path");
 
-
 async function main() {
     const readmeTemplate = (
         await fs.readFile(path.join(process.cwd(), "./README.template.md"))
